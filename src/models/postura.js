00cbf585dec4c0f539298acc53a5bd4412ec464d
MutsuM <marko.pachas@gmail.com>
2025-03-29 08:20:22 -0500

    ajustes despliege servidor

--- src/models/postura.js
+++ src/models/postura.js
@@ -1,13 +1,13 @@
 const pool = require('../config/db');
 
 const getLastPostura = async () => {
-    const { rows } = await pool.query('SELECT postura FROM store.posturas ORDER BY id DESC LIMIT 1');
+    const { rows } = await pool.query('SELECT postura FROM public.postura ORDER BY id DESC LIMIT 1');
     return rows[0];
 };
 
 const createPostura = async (postura) => {
     const { rows } = await pool.query(
-        'INSERT INTO store.posturas (postura) VALUES ($1) RETURNING id',
+        'INSERT INTO public.postura (postura) VALUES ($1) RETURNING id',
         [postura]
     );
     return rows[0].id;
